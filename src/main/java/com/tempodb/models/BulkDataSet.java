25f2f0b40d203819b0df73d30f52edf2f5413a80
Mike Yagley <myagley@gmail.com>
2012-04-01 17:04:26 -0500

    Add more documentation

--- src/main/java/com/tempodb/models/BulkDataSet.java
+++ src/main/java/com/tempodb/models/BulkDataSet.java
@@ -8,10 +8,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.joda.time.DateTime;
 
 
+/**
+ *  Set of data to send for a bulk write. This encapsulates the timestamp and list of BulkPoints
+ */
 public class BulkDataSet {
     private final DateTime timestamp;
     private final List<BulkPoint> data;
 
+    /**
+     *  @param timestamp The timestamp to write the datapoints at
+     *  @param data A list of BulkPoints to write
+     */
     @JsonCreator
     public BulkDataSet(@JsonProperty("t") DateTime timestamp, @JsonProperty("data") List<BulkPoint> data) {
         this.timestamp = timestamp;
