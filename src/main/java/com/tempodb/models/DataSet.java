3d30043c044d0a2cb84284a6d06711337e442914
Mike Yagley <myagley@gmail.com>
2012-04-01 17:16:18 -0500

    Revert "Revert "Add more documentation""
    This reverts commit d0875d44db551d86c31ed834c117c71e7154dcb0.

--- src/main/java/com/tempodb/models/DataSet.java
+++ src/main/java/com/tempodb/models/DataSet.java
@@ -10,6 +10,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.joda.time.DateTime;
 
 
+/**
+ *  Respresents data from a time range of a series. This is essentially a list
+ *  of DataPoints with some added metadata. This is the object returned from a query.
+ *  The DataSet contains series metadata, the start/end times for the queried range,
+ *  a list of the DataPoints and a statistics summary table. The Summary table contains
+ *  statistics for the time range (sum, mean, min, max, count, etc.)
+ */
 public class DataSet {
     private final Series series;
     private final DateTime start;
@@ -17,6 +24,13 @@ public class DataSet {
     private final List<DataPoint> data;
     private final Map<String, Number> summary;
 
+    /**
+     *  @param series series metadata (Series)
+     *  @param start start time for the queried range (DateTime)
+     *  @param end end time for the queried range (DateTime)
+     *  @param data datapoints (List of DataPoints)
+     *  @param summary a summary table of statistics for the queried range (Map)
+     */
     @JsonCreator
     public DataSet(@JsonProperty("series") Series series, @JsonProperty("start") DateTime start,
             @JsonProperty("end") DateTime end, @JsonProperty("data") List<DataPoint> data,
