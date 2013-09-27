920101c6904eb10a4b89305196ece0dc94fd4fb5
Mike Yagley <myagley@gmail.com>
2013-09-27 11:53:35 -0500

    Add integration test framework

---
+++ src/test/java/com/tempodb/it/ClientIT.java
@@ -0,0 +1,24 @@
+package com.tempodb.it;
+
+import org.joda.time.DateTime;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import com.tempodb.*;
+
+
+public class ClientIT {
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+  @Test
+  public void testEquals() {
+    assertEquals(1, 1);
+  }
+}
